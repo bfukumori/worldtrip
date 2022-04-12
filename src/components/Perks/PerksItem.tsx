cd3669883d84e966648ea3bfa46083dacb83d0bb
Bruno Fukumori <brunofukumori@gmail.com>
2022-04-12 15:45:40 -0300

    fix icon in perksitem

--- src/components/Perks/PerksItem.tsx
+++ src/components/Perks/PerksItem.tsx
@@ -1,4 +1,4 @@
-import { Flex, Image, Text, useBreakpointValue } from "@chakra-ui/react";
+import { Flex, Icon, Image, Text, useBreakpointValue } from "@chakra-ui/react";
 
 interface PerksItemProps {
   imgSrc: string;
@@ -14,7 +14,11 @@ export const PerksItem = ({ imgAlt, imgSrc, title }: PerksItemProps) => {
   return (
     <Flex direction="column" justify="end" align="center">
       {isWideVersion && <Image src={imgSrc} alt={imgAlt} />}
-      <Text mt={6} fontSize={["18px", "24px"]} fontWeight={[500, 600]} lineHeight={["27px", "36px"]} _before={!isWideVersion && { content: `""`, width: "8px", height: "8px", bgColor: "yellow.900", display: "inline-block", verticalAlign: "middle", mr: "8px", borderRadius: "50%" }}>
+      <Text mt={6} fontSize={["18px", "24px"]} fontWeight={[500, 600]} lineHeight={["27px", "36px"]}>
+        {
+          !isWideVersion && <Icon boxSize={2} color="yellow.900" bgColor="yellow.900" borderRadius="4px" mr="8px" />
+        }
+
         {title}
       </Text>
     </Flex>
